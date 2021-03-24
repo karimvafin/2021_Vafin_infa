7fac453709456f7ef8d9c29ca75e87ba4e5e7fd6
karimvafin <karim.vafin@mail.ru>
2021-03-24 12:38:54 +0300

    modification

--- lab5/classes.py
+++ lab5/classes.py
@@ -1,10 +1,6 @@
 import pygame
 from random import randrange as rnd, choice
 import math
-pygame.init()
-
-FPS = 60
-screen = pygame.display.set_mode((800, 600))
 
 RED = (255, 0, 0)
 WHITE = (255, 255, 255)
@@ -15,8 +11,8 @@ MAGENTA = (255, 0, 255)
 CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
-
-EXIT = False
+screen = pygame.display.set_mode((800, 600))
+bullet = 0
 
 
 class Ball:
@@ -34,7 +30,7 @@ class Ball:
         self.vy = 0
         self.color = choice(['blue', 'green', 'red', 'brown'])
         self.id = [self.x, self.y, self.r]
-        self.live = 130
+        self.live = 150
 
     def draw_ball(self):
         pygame.draw.circle(screen, self.color, (self.x, self.y), self.r)
@@ -120,8 +116,7 @@ class Gun:
         Происходит при отпускании кнопки мыши.
         Начальные значения компонент скорости мяча vx и vy зависят от положения мыши.
         """
-        global balls, bullet
-        bullet += 1
+        global balls
         new_ball = Ball()
         new_ball.r += 5
         self.an = math.atan((event.pos[1]-new_ball.y) / (event.pos[0]-new_ball.x))
@@ -142,15 +137,12 @@ class Gun:
             self.color = RED
         else:
             self.color = BLACK
-        # self.x = 20 + max(self.f2_power, 20) * math.cos(self.an)
-        # self.y = 450 + max(self.f2_power, 20) * math.sin(self.an)
         self.C = (self.x + self.f2_power * 25 * math.cos(self.an) + 5 * math.sin(self.an), self.y - self.f2_power
                   * 25 * math.sin(self.an) + 5 * math.cos(self.an))
         self.D = (self.x + self.f2_power * 25 * math.cos(self.an) - 5 * math.sin(self.an), self.y - self.f2_power
                   * 25 * math.sin(self.an) - 5 * math.cos(self.an))
         self.A = (self.x - 5 * math.sin(self.an), self.y - 5 * math.cos(self.an))
         self.B = (self.x + 5 * math.sin(self.an), self.y + 5 * math.cos(self.an))
-        # self.draw_gun()
 
     def power_up(self):
         if self.f2_on:
@@ -163,21 +155,46 @@ class Gun:
 
 class Target:
 
-    def __init__(self):
+    def __init__(self, type):
+        self.angle = 1
+        self.v = 3
         self.live = 1
         self.x = 0
         self.y = 0
         self.r = 0
-        self.color = RED
+        self.color = choice(COLORS)
+        self.type = type
 
     def draw_target(self):
-        pygame.draw.circle(screen, self.color, (self.x, self.y), self.r)
+        if self.type == 'circle':
+            pygame.draw.circle(screen, self.color, (self.x, self.y), self.r)
+        if self.type == 'triangle':
+            pygame.draw.polygon(screen, self.color, ((self.x, self.y - self.r * math.sqrt(3) / 3),
+                                    (self.x - self.r / 2, self.y + math.sqrt(3) * self.r / 6),
+                                    (self.x + self.r / 2, self.y + math.sqrt(3) * self.r / 6)))
 
     def new_target(self):
         """ Инициализация новой цели. """
         self.x = rnd(600, 750)
         self.y = rnd(300, 520)
         self.r = rnd(5, 50)
+        self.draw_target()
+
+    def move(self):
+        """Переместить мяч по прошествии единицы времени.
+
+        Метод описывает перемещение мяча за один кадр перерисовки. То есть, обновляет значения
+        self.x и self.y с учетом скоростей self.vx и self.vy, силы гравитации, действующей на мяч,
+        и стен по краям окна (размер окна 800х600).
+        """
+        if self.x > 790 - self.r or self.x < 400 + self.r + 10:
+            self.v = - self.v
+        if self.y > 570 - self.r or self.y < 200:
+            self.v = - self.v
+
+        self.angle += rnd(-15, 15) / 100
+        self.x += self.v * math.cos(self.angle)
+        self.y += self.v * math.sin(self.angle)
         self.draw_target()
 
     def hit(self):
@@ -187,91 +204,4 @@ class Target:
         self.draw_target()
 
 
-def print_text(txt, color, position, size):
-    f1 = pygame.font.Font('/Users/karimvafin/opt/anaconda3/pkgs/matplotlib-base-3.3.2-py38h181983e_0/lib/python3.8/'
-                          'site-packages/matplotlib/mpl-data/fonts/ttf/DejaVuSansMono-Oblique.ttf', size)
-    text1 = f1.render(txt, True,
-                      color, WHITE)
-
-    screen.blit(text1, position)
-
-
-t1 = Target()
-g1 = Gun()
-bullet = 0
 balls = []
-hit = False
-count = 0
-text = ''
-points = 0
-
-pygame.display.update()
-clock = pygame.time.Clock()
-
-
-def new_game():
-    global g1, t1, screen, balls, bullet, hit, count, text, EXIT, points
-
-    t1.new_target()
-    bullet = 0
-    balls = []
-    screen.fill(WHITE)
-    finished = False
-    t1.live = 1
-    text = ''
-    count = 0
-
-    while not finished:
-        clock.tick(FPS)
-        if not balls and not t1.live:
-            finished = True
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                finished = True
-                EXIT = True
-            if event.type == pygame.MOUSEBUTTONUP:
-                g1.fire2_end(event)
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                g1.fire2_start()
-
-        g1.targetting()
-        g1.power_up()
-        g1.draw_gun()
-        t1.draw_target()
-        print_text(text, BLACK, (200, 200), 20)
-        print_text(str(points), BLACK, (20, 20), 30)
-
-        for b in balls:
-            b.move()
-            if b.hittest(t1) and t1.live:
-                t1.live = 0
-                t1.hit()
-                points += 1
-                text = 'Вы уничтожили цель за ' + str(bullet) + ' выстрелов'
-                print_text(text, BLACK, (200, 200), 20)
-                hit = True
-
-            if b.check_alive():
-                b.delete_ball()
-                balls.pop(0)
-
-        if hit:
-            count += 1
-        if count == 70:
-            for i in range(len(balls)):
-                balls[i].delete_ball()
-            balls = []
-            count = 0
-            hit = False
-            break
-
-        pygame.display.update()
-        screen.fill(WHITE)
-
-
-while not EXIT:
-    new_game()
-    screen.fill(WHITE)
-
-pygame.quit()
